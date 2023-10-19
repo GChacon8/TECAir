d5345b37e90bf073fd00e78112caa7c0d8a34b95
Dcastroe10 <dcastroeli@gmail.com>
2023-10-19 03:07:32 -0600

    Update ClientView

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import ico from "./Images/Ico.jpg"
 
 
 import './Css/Main.css';  
@@ -14,34 +13,11 @@ import Sucursal_view from './vistas/SucursalRegister';
 import Sala_view from './vistas/SalaRegister';
 import Proyection_view from './vistas/ProyectionRegister';
 
+
 function App() {
   return (
     <Router>
-      <nav className="navbar navbar-expand-lg  navbar-dark justify-content-between">
-        <div className="container">
-          <a className="navbar-brand" href="#">
-            <img src={ico} width="50" height="50" alt=""></img>TecAir
-          </a>
-
-          <ul className="navbar-nav ml-auto d-flex">
-            <li className="nav-item">
-              <Link className="nav-link" to="/iniciar_sesion">TEST</Link>
-            </li>
-
-            <li className="nav-item">
-              <Link className="nav-link" to="/ClientView">
-                Cliente
-              </Link>
-            </li>
-
-            <li className="nav-item">
-              <Link className="nav-link" to="/Login">
-                Iniciar Sesión
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
+      
 
 
   
@@ -49,14 +25,18 @@ function App() {
 
       <div className="container">
         <Routes>
-          <Route path="/ClientView" element={<Client_view />} />
+          <Route path="/TecAir/Login_Admin" element={<Login_view />} /> 
+          
+          <Route path="/2" element={<Client_view />} />
+          <Route path="/3" element={<Register_view />} />
+          <Route path="/4" element={<Sucursal_view />} />
+          <Route path="/5" element={<Sala_view />} />
+          <Route path="/6" element={<Proyection_view />} />
+          <Route path="/7" element={<Admin_view />} />
+
+
           <Route path="/CineTec_Prototipo" element={<Client_view />} />
-          <Route path="/MovieRegister" element={<Register_view />} />
-          <Route path="/SucursalRegister" element={<Sucursal_view />} />
-          <Route path="/SalaRegister" element={<Sala_view />} />
-          <Route path="/ProyectionRegister" element={<Proyection_view />} />
-          <Route path="/TecAir/Login_Admin" element={<Login_view />} />
-          <Route path="/AdminView" element={<Admin_view />} />
+
         </Routes>
       </div>
     </Router>
--- src/vistas/Client_view.js
+++ src/vistas/Client_view.js
@@ -1,33 +1,137 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { useNavigate } from 'react-router-dom';
-
+import ico from "../Images/Ico.jpg"
 
 function Client_view() {
-  const navigate = useNavigate();
+  const [origin, setOrigin] = useState('');
+  const [destination, setDestination] = useState('');
+  const [departureDate, setDepartureDate] = useState('');
+  const [returnDate, setReturnDate] = useState(''); // Nuevo campo de fecha de regreso
 
-  //por si quiero llamar a alguna función desde un button
-  const handleButtonClick = () => {
-    console.log('SOY UN BOTÓN');
+  const [originOptions, setOriginOptions] = useState([]);
+  const [DestOptions, setDestOptions] = useState([]);
+
+  // Función para manejar el envío del formulario, LLAMAR AL API AQUÍ PARA ENVIAR DATOS
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log('Origen:', origin);
+    console.log('Destino:', destination);
+    console.log('Fecha de Salida:', departureDate);
+    console.log('Fecha de Regreso:', returnDate); // Nuevo campo de fecha de regreso
   };
 
+  // OBTENER DATOS DEL API AQUÍ
+  const Data_for_reserve = () => {
+    const origenes = ['Ciudad A', 'Ciudad B', 'Ciudad C', 'Ciudad D'];
+    const destinos = ['Ciudad E', 'Ciudad E', 'Ciudad F', 'Ciudad G'];
+    setOriginOptions(origenes);
+    setDestOptions(destinos);
+  };
 
-  //en los select de las lineas 29 par abajo se debe de accesar a la DB y modificar las opciones!!!
+  useEffect(() => {
+    Data_for_reserve();
+  }, []);
+
   return (
+    <div className='container'>
+      <div>
+        <nav className="navbar navbar-expand-lg  navbar-dark justify-content-between navbarr">
+          <div className="container">
+            <a className="navbar-brand" href="#">
+              <img src={ico} width="50" height="50" alt="" />
+              TecAir
+            </a>
 
-    <div
-      style={{
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        minHeight: '100vh',
-        width: '100%',
-      }}>
-     
-     <h1>test</h1>
+            <ul className="navbar-nav ml-auto d-flex">
+              <li className="nav-item">
+                <Link className="nav-link" to="/iniciar_sesion">
+                  TEST
+                </Link>
+              </li>
+
+              <li className="nav-item">
+                <Link className="nav-link" to="/ClientView">
+                  Cliente
+                </Link>
+              </li>
+
+              <li className="nav-item">
+                <Link className="nav-link" to="/TecAir/Login_Admin">
+                  Iniciar Sesión!
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+
+      <div className='reserve'>
+        <h1>Selección de Vuelo</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="form-group espace">
+            <label htmlFor="origin">Origen:</label>
+            <select
+              className="form-control"
+              id="origin"
+              value={origin}
+              onChange={(e) => setOrigin(e.target.value)}
+            >
+              <option value="">Origen</option>
+              {originOptions.map((option) => (
+                <option key={option} value={option}>
+                  {option}
+                </option>
+              ))}
+            </select>
+          </div>
+
+          <div className="form-group espace">
+            <label htmlFor="destination">Destino:</label>
+            <select
+              className="form-control"
+              id="destination"
+              value={destination}
+              onChange={(e) => setDestination(e.target.value)}
+            >
+              <option value="">Destino</option>
+              {DestOptions.map((option) => (
+                <option key={option} value={option}>
+                  {option}
+                </option>
+              ))}
+            </select>
+          </div>
+
+          <div className="form-group espace">
+            <label htmlFor="departureDate">Fecha de Salida:</label>
+            <input
+              type="date"
+              className="form-control"
+              id="departureDate"
+              value={departureDate}
+              onChange={(e) => setDepartureDate(e.target.value)}
+            />
+          </div>
+
+          <div className="form-group espace">
+            <label htmlFor="returnDate">Fecha de Regreso:</label>
+            <input
+              type="date"
+              className="form-control"
+              id="returnDate"
+              value={returnDate}
+              onChange={(e) => setReturnDate(e.target.value)}
+            />
+          </div>
+
+          <button type="submit" className="btn btn-primary">
+            Reservar Vuelo
+          </button>
+        </form>
+      </div>
     </div>
-    
   );
 }
 
