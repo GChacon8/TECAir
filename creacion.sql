dc9c52ec8c7220b7e93fca048c335fad07b461fc
eduardocv30 <jecruz@estudiantec.cr>
2023-10-20 19:19:00 -0600

    mayusculas

--- creacion.sql
+++ creacion.sql
@@ -1,124 +1,124 @@
 -- Tabla de usuario
-CREATE TABLE Usuario (
-    Correo VARCHAR(40) NOT NULL,
-    Contraseña VARCHAR(20) NOT NULL,
-    Pasaporte VARCHAR(20),
-    Nombre_1 VARCHAR(30),
-    Nombre_2 VARCHAR(30),
-    Apellido_1 VARCHAR(30),
-    Apellido_2 VARCHAR(30),
-    Telefono INT,
-    Check_in BOOL,
-    PRIMARY KEY (Correo)
+CREATE TABLE "Usuario" (
+    "Correo" VARCHAR(40) NOT NULL,
+    "Contraseña" VARCHAR(20) NOT NULL,
+    "Pasaporte" VARCHAR(20),
+    "Nombre_1" VARCHAR(30),
+    "Nombre_2" VARCHAR(30),
+    "Apellido_1" VARCHAR(30),
+    "Apellido_2" VARCHAR(30),
+    "Telefono" INT,
+    "Check_in" BOOL,
+    PRIMARY KEY ("Correo")
 );
 
 -- Tabla del avión
-CREATE TABLE Avion (
-    Matricula VARCHAR(40),
-    Modelo VARCHAR(255),
-    Capacidad INT,
-    PRIMARY KEY (Matricula)
+CREATE TABLE "Avion" (
+    "Matricula" VARCHAR(40),
+    "Modelo" VARCHAR(255),
+    "Capacidad" INT,
+    PRIMARY KEY ("Matricula")
 );
 
-CREATE TABLE Aero (
-    ID VARCHAR(60) NOT NULL,
-    Puerta VARCHAR(30),
-    Ciudad VARCHAR(60),
-    Nombre VARCHAR(255),
-    PRIMARY KEY (ID)
+CREATE TABLE "Aero" (
+    "ID" VARCHAR(60) NOT NULL,
+    "Puerta" VARCHAR(30),
+    "Ciudad" VARCHAR(60),
+    "Nombre" VARCHAR(255),
+    PRIMARY KEY ("ID")
 );
 
-CREATE TABLE Viaje (
-    ID INT NOT NULL,
-    Estado BOOL,
-    Fin VARCHAR(60),
-    Inicio VARCHAR(60),
-    PRIMARY KEY (ID),
-    FOREIGN KEY (Fin) REFERENCES Aero(ID),
-    FOREIGN KEY (Inicio) REFERENCES Aero(ID)
+CREATE TABLE "Viaje" (
+    "ID" INT NOT NULL,
+    "Estado" BOOL,
+    "Fin" VARCHAR(60),
+    "Inicio" VARCHAR(60),
+    PRIMARY KEY ("ID"),
+    FOREIGN KEY ("Fin") REFERENCES "Aero"("ID"),
+    FOREIGN KEY ("Inicio") REFERENCES "Aero"("ID")
 );
 
-CREATE TABLE Ejecucion (
-    ID INT NOT NULL,
-    Hora TIME,
-    Precio INT,
-    Matricula_Avion VARCHAR(40),
-    PRIMARY KEY (ID),
-    FOREIGN KEY (Matricula_Avion) REFERENCES Avion(Matricula)
+CREATE TABLE "Ejecucion" (
+    "ID" INT NOT NULL,
+    "Hora" TIME,
+    "Precio" INT,
+    "Matricula_Avion" VARCHAR(40),
+    PRIMARY KEY ("ID"),
+    FOREIGN KEY ("Matricula_Avion") REFERENCES "Avion"("Matricula")
 );
 
 -- Tabla de estudiantes
-CREATE TABLE Estudiante (
-    Carnet VARCHAR(30) NOT NULL,
-    Universidad VARCHAR(60),
-    Correo_Usuario VARCHAR(40) NOT NULL,
-    PRIMARY KEY (Carnet),
-    FOREIGN KEY (Correo_Usuario) REFERENCES Usuario(Correo)
+CREATE TABLE "Estudiante" (
+    "Carnet" VARCHAR(30) NOT NULL,
+    "Universidad" VARCHAR(60),
+    "Correo_Usuario" VARCHAR(40) NOT NULL,
+    PRIMARY KEY ("Carnet"),
+    FOREIGN KEY ("Correo_Usuario") REFERENCES "Usuario"("Correo")
 );
 
 -- Tabla de maletas
-CREATE TABLE Maleta (
-    Numero INT NOT NULL,
-    Peso INT,
-    Color VARCHAR(20),
-    Matricula_Avion VARCHAR(30),
-    Correo_Usuario VARCHAR(40),
-    PRIMARY KEY (Numero),
-    FOREIGN KEY (Matricula_Avion) REFERENCES Avion(Matricula),
-    FOREIGN KEY (Correo_Usuario) REFERENCES Usuario(Correo)
+CREATE TABLE "Maleta" (
+    "Numero" INT NOT NULL,
+    "Peso" INT,
+    "Color" VARCHAR(20),
+    "Matricula_Avion" VARCHAR(30),
+    "Correo_Usuario" VARCHAR(40),
+    PRIMARY KEY ("Numero"),
+    FOREIGN KEY ("Matricula_Avion") REFERENCES "Avion"("Matricula"),
+    FOREIGN KEY ("Correo_Usuario") REFERENCES "Usuario"("Correo")
 );
 
 -- Tabla de asientos
-CREATE TABLE Asiento (
-    Numero INT NOT NULL,
-    X INT,
-    Y INT,
-    Ocupado BOOL,
-    Correo_Usuario VARCHAR(40),
-    Matricula_Avion VARCHAR(40),
-    PRIMARY KEY (Numero),
-    FOREIGN KEY (Matricula_Avion) REFERENCES Avion(Matricula),
-    FOREIGN KEY (Correo_Usuario) REFERENCES Usuario(Correo)
+CREATE TABLE "Asiento" (
+    "Numero" INT NOT NULL,
+    "X" INT,
+    "Y" INT,
+    "Ocupado" BOOL,
+    "Correo_Usuario" VARCHAR(40),
+    "Matricula_Avion" VARCHAR(40),
+    PRIMARY KEY ("Numero"),
+    FOREIGN KEY ("Matricula_Avion") REFERENCES "Avion"("Matricula"),
+    FOREIGN KEY ("Correo_Usuario") REFERENCES "Usuario"("Correo")
 );
 
 -- Tabla de la entidad débil promoción
-CREATE TABLE Promocion (
-    Precio INT,
-    Fecha_Inicio DATE,
-    Fecha_Final DATE,
-    Imagen VARCHAR(1000),
-    ID_Ejecucion INT,
-    FOREIGN KEY (ID_Ejecucion) REFERENCES Ejecucion(ID)
+CREATE TABLE "Promocion" (
+    "Precio" INT,
+    "Fecha_Inicio" DATE,
+    "Fecha_Final" DATE,
+    "Imagen" VARCHAR(1000),
+    "ID_Ejecucion" INT,
+    FOREIGN KEY ("ID_Ejecucion") REFERENCES "Ejecucion"("ID")
 );
 
 -- Tabla auxiliar que relaciona vuelos con usuarios
-CREATE TABLE EjecucionXUsuario (
-    ID_Ejecucion INT,
-    Correo_Usuario VARCHAR(40),
-    FOREIGN KEY (ID_Ejecucion) REFERENCES Ejecucion(ID),
-    FOREIGN KEY (Correo_Usuario) REFERENCES Usuario(Correo)
+CREATE TABLE "EjecucionXUsuario" (
+    "ID_Ejecucion" INT,
+    "Correo_Usuario" VARCHAR(40),
+    FOREIGN KEY ("ID_Ejecucion") REFERENCES "Ejecucion"("ID"),
+    FOREIGN KEY ("Correo_Usuario") REFERENCES "Usuario"("Correo")
 );
 
-CREATE TABLE ViajeXEjecuciones (
-    ID_Ejecucion INT,
-    ID_Viaje INT,
-    FOREIGN KEY (ID_Ejecucion) REFERENCES Ejecucion(ID),
-    FOREIGN KEY (ID_Viaje) REFERENCES Viaje(ID)
+CREATE TABLE "ViajeXEjecuciones" (
+    "ID_Ejecucion" INT,
+    "ID_Viaje" INT,
+    FOREIGN KEY ("ID_Ejecucion") REFERENCES "Ejecucion"("ID"),
+    FOREIGN KEY ("ID_Viaje") REFERENCES "Viaje"("ID")
 );
 
 -- Tabla de las tarjetas del usuario
-CREATE TABLE Tarjeta (
-    Numero VARCHAR(20) NOT NULL,
-    CS INT,
-    Vencimiento DATE,
-    Correo_Usuario VARCHAR(40),
-    PRIMARY KEY (Numero),
-    FOREIGN KEY (Correo_Usuario) REFERENCES Usuario(Correo)
+CREATE TABLE "Tarjeta" (
+    "Numero" VARCHAR(20) NOT NULL,
+    "CS" INT,
+    "Vencimiento" DATE,
+    "Correo_Usuario" VARCHAR(40),
+    PRIMARY KEY ("Numero"),
+    FOREIGN KEY ("Correo_Usuario") REFERENCES "Usuario"("Correo")
 );
 
-CREATE TABLE Escala (
-    ID_Viaje INT NOT NULL,
-    ID_Aero VARCHAR(60) NOT NULL,
-    FOREIGN KEY (ID_Viaje) REFERENCES Viaje(ID),
-    FOREIGN KEY (ID_Aero) REFERENCES Aero(ID)
+CREATE TABLE "Escala" (
+    "ID_Viaje" INT NOT NULL,
+    "ID_Aero" VARCHAR(60) NOT NULL,
+    FOREIGN KEY ("ID_Viaje") REFERENCES "Viaje"("ID"),
+    FOREIGN KEY ("ID_Aero") REFERENCES "Aero"("ID")
 );
--- populacion_01.sql
+++ populacion_01.sql
@@ -1,48 +1,41 @@
 -- Insertar datos en la tabla Aero
-INSERT INTO Aero (ID, Ciudad, Nombre)
+INSERT INTO "Aero" ("ID", "Ciudad", "Nombre")
 VALUES
-    ('SJO', 'San José', 'Aeropuerto Internacional Juan Santamaría'),
-    ('BAI', 'Buenos Aires', 'Aeropuerto de Buenos Aires'),
-    ('MIA', 'Miami', 'Aeropuerto Internacional de Miami'),
-    ('MEX', 'Ciudad de Mexico', 'Aeropuerto Internacional Benito Juárez de la Ciudad de Mexico'),
-    ('PTY', 'Ciudad de Panama', 'Aeropuerto Internacional de Tocumen');
-
+('SJO', 'San José', 'Aeropuerto Internacional Juan Santamaría'),
+('BAI', 'Buenos Aires', 'Aeropuerto de Buenos Aires'),
+('MIA', 'Miami', 'Aeropuerto Internacional de Miami'),
+('MEX', 'Ciudad de Mexico', 'Aeropuerto Internacional Benito Juárez de la Ciudad de Mexico'),
+('PTY', 'Ciudad de Panama', 'Aeropuerto Internacional de Tocumen');
 -- Insertar datos en la tabla Viaje
-INSERT INTO Viaje (ID, Estado)
+INSERT INTO "Viaje" ("ID", "Estado")
 VALUES
-    (1, FALSE),
-    (2, FALSE),
-    (3, FALSE);
-
+(1, FALSE),
+(2, FALSE),
+(3, FALSE);
 -- Vuelo directo a Miami
-UPDATE Viaje
-SET Inicio = 'SJO', Fin = 'MIA'
-WHERE ID = 1;
-
+UPDATE "Viaje"
+SET "Inicio" = 'SJO', "Fin" = 'MIA'
+WHERE "ID" = 1;
 -- Vuelo directo a Argentina
-UPDATE Viaje
-SET Inicio = 'SJO', Fin = 'BAI'
-WHERE ID = 2;
-
+UPDATE "Viaje"
+SET "Inicio" = 'SJO', "Fin" = 'BAI'
+WHERE "ID" = 2;
 -- Vuelo a Miami con escalas
-UPDATE Viaje
-SET Inicio = 'SJO', Fin = 'MIA'
-WHERE ID = 3;
-
+UPDATE "Viaje"
+SET "Inicio" = 'SJO', "Fin" = 'MIA'
+WHERE "ID" = 3;
 -- Insertar datos en la tabla Escala
-INSERT INTO Escala (ID_Viaje, ID_Aero)
+INSERT INTO "Escala" ("ID_Viaje", "ID_Aero")
 VALUES (3, 'PTY');
-
 -- Usuario administrador
-INSERT INTO Usuario (Correo, Contraseña, Nombre_1, Apellido_1, Pasaporte)
+INSERT INTO "Usuario" ("Correo", "Contraseña", "Nombre_1", "Apellido_1", "Pasaporte")
 VALUES ('admin@eropuerto.com', '12345678', 'Jose', 'Cruz', '123456789');
-
 -- Insertar datos en la tabla Avion
-INSERT INTO Avion (Matricula, Modelo, Capacidad)
+INSERT INTO "Avion" ("Matricula", "Modelo", "Capacidad")
 VALUES
-    ('B74701', 'Boeing747', 120),
-    ('A32001', 'Airbus A320', 120),
-    ('B78701', 'Boeing 787 Dreamliner', 120);
+('B74701', 'Boeing747', 120),
+('A32001', 'Airbus A320', 120),
+('B78701', 'Boeing 787 Dreamliner', 120);
 
 
 
