907b7ef562c6daa3608e4c6b994230a2b1869db0
Noemí Vargas <mimi2002@estudiantec.cr>
2023-10-22 23:14:37 -0600

    Client and Admin on IIS

--- Client/Program.cs
+++ Client/Program.cs
@@ -13,7 +13,7 @@ builder.Services.AddDbContext<TodoContext>(opt =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (!app.Environment.IsProduction())
 {
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
